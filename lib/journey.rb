455527f560f642584e1b4b8c8afaca2cf25496ae
andy jenkins <andygjenkins@gmail.com>
2016-10-07 11:50:53 +0100

    Add further tests for Journey class

--- lib/journey.rb
+++ lib/journey.rb
@@ -1,16 +1,19 @@
 class Journey
 
   attr_reader :entry_station, :exit_station, :fare
-  
+
   PENALTY_FARE = 6
 
-  def initialize(entry_station = nil, exit_station = nil)
-    @entry_station = entry_station
-    @exit_station = exit_station
+  def initialize(station = nil)
+    @entry_station = station
     @complete = false
     @fare = PENALTY_FARE
   end
-  
+
+  def finish(station)
+    self
+  end
+
   def complete?
     @complete
   end
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -1,48 +1,33 @@
 require 'spec_helper'
 
 describe Journey do
-  
+
   subject(:journey) { Journey.new }
   let(:station)  {   double(:station)  }
-  
+
+
   describe 'initizalization' do
-    
     it 'knows if a journey is not complete' do
-      expect(journey).not_to be_complete    
-     end  
+      expect(journey).not_to be_complete
+     end
 
     it 'has a penalty fare by default' do
       expect(journey.fare).to eq Journey::PENALTY_FARE
     end
+
+  end
+
+  describe '#finish' do
+    it 'returns itself when exiting a journey' do
+      expect(journey.finish(station)).to eq journey
+    end
+  end
+
+  context 'given an entry_station' do
+    subject(:journey) { Journey.new(station) }
+
+    it 'has an entry station' do
+      expect(journey.entry_station).to eq station
+    end
   end
 end
-
-
-
-
-=begin
-  describe '#initizalization' do
-    
-    it 'should return nil if no arguments are passed to entry_station' do
-      expect(card.journey.entry_station).to eq nil
-    end
-    
-    it 'should return nil if no arguments are passed to exit_station' do
-      expect(card.journey.exit_station).to eq nil
-    end
-    
-  end
-
-
-  describe '#in_journey?' do
-
-    it 'expects in_journey to equal false be default' do
-      expect(card.journey.in_journey?).to be false
-    end
-
-    it 'is expected to check status based on entry station' do
-      card.touch_in(station)
-      expect(card.journey.in_journey?).to be true
-    end
-  end
-=end
